22b6aa3c880240ba36c40094243d9b4ffb2ef3db
segmentation_fault <shibani.raum@gmail.com>
2023-12-12 17:53:04 +0100

    Address command line usage errors
    If applied, this commit will attempt to address all command line usage
    errors

--- path_finding/algorithms/a_star/main.py
+++ path_finding/algorithms/a_star/main.py
@@ -4,9 +4,17 @@ sys.path.append('../../../')
 from path_finding.API import API
 
 def main():
-    file = sys.argv[1]
-
     if len(sys.argv) == 2:
+        try:
+            f = open(sys.argv[1])
+        except FileNotFoundError:
+            print("\nUSAGE: python main.py maze_file_path")
+            print("\nTo perform unit tests: python main.py maze_file_path --unittest number_of_test_iterations\n")
+
+            return 1
+        
+        file = sys.argv[1]
+
         maze, coord_space, start, end, max_x, max_y = API.init(file)
         
         start_time = time.time()
@@ -19,7 +27,7 @@ def main():
         API.visualise(maze)
         print(f"Execution time: {end_time - start_time}")
 
-    elif sys.argv[2] == "--unittest":
+    elif len(sys.argv) > 2 and sys.argv[2] == "--unittest":
 
         if len(sys.argv) == 4:
             API.unit_test(int(sys.argv[3]), API.a_star, file)
--- path_finding/algorithms/dijkstra/main.py
+++ path_finding/algorithms/dijkstra/main.py
@@ -5,12 +5,18 @@ from path_finding.API import API
 
     
 def main():
-    file = sys.argv[1]
-
-    maze, coord_space, start, end, max_x, max_y = API.init(file)
-
     if len(sys.argv) == 2:
+        try:
+            f = open(sys.argv[1])
+        except FileNotFoundError:
+            print("\nUSAGE: python main.py maze_file_path")
+            print("\nTo perform unit tests: python main.py maze_file_path --unittest number_of_test_iterations\n")
+
+            return 1
         
+        file = sys.argv[1]
+        maze, coord_space, start, end, max_x, max_y = API.init(file)
+
         start_time = time.time()
         path = API.dijkstra(start, end, coord_space, maze)
         end_time = time.time() 
@@ -21,7 +27,7 @@ def main():
         API.visualise(maze)
         print(f"Execution time: {end_time - start_time}")
 
-    elif sys.argv[2] == "--unittest":
+    elif len(sys.argv) > 2 and sys.argv[2] == "--unittest":
 
         if len(sys.argv) == 4:
             API.unit_test(int(sys.argv[3]), API.dijkstra, file)
